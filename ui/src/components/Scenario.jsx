3c8c016a9187cf0a0fb12626a829d4279f06e10d
Anvita2110 <110974190+Anvita2110@users.noreply.github.com>
2025-01-18 15:11:22 +0800

    Set up frontend page

---
+++ ui/src/components/Scenario.jsx
@@ -0,0 +1,15 @@
+import { CameraControls, Environment } from "@react-three/drei";
+import { useEffect, useRef } from "react";
+
+export const Scenario = () => {
+  const cameraControls = useRef();
+  useEffect(() => {
+    cameraControls.current.setLookAt(0, 2.2, 5, 0, 1.0, 0, true);
+  }, []);
+  return (
+    <>
+      <CameraControls ref={cameraControls} />
+      <Environment preset="sunset" />
+    </>
+  );
+};
